2997ff7e246432aadbc4602f92c6d4a1aa250840
Энтони Диллинджер <tonydillinger@MacBook-Air-Entoni.local>
2020-05-01 17:00:53 +0300

    add mil

---
+++ Mili.java
@@ -0,0 +1,8 @@
+public class Mili {
+    public static void main(String[] args) {
+        int spent = 200; // потрачено на билет
+        int pcice = 20;  // цена за 1 милю
+        int result = spent / pcice; // количество миль
+        System.out.println(result);
+    }
+}
